2695293cae1db7e68b21e1bd0be841cdc67ba0b0
Julian Jorgensen <me@julianjorgensen.com>
2017-09-13 10:31:40 +0800

    Fixing estimate webhook emails

--- routes/qboWebhook.js
+++ routes/qboWebhook.js
@@ -1,12 +1,14 @@
 let express = require('express');
 let router = express.Router();
 let app = require('../app');
-let _ = require('lodash');
 
 let util = require('../util/util');
 let ENV_CONFIG = util.getEnvConfig();
 
-let invoiceMailer = require('../admin/emails/billingItem/init');
+let {
+  mailItem
+} = requre('./utils/qbo');
+
 let invoiceReminder = require('../admin/crons/invoiceReminder/init');
 
 router.route('/')
@@ -36,61 +38,27 @@ router.route('/')
         let eventType = event.operation.toLowerCase();
         console.log('eventType: ', eventType);
 
-        let eventId = event.id;
+        let itemId = event.id;
 
         // If its an invoice
         if (eventName === 'invoice') {
-          // get invoice details
-          qbo.getInvoice(eventId, (err, invoice) => {
-            console.log('got invoice details from webhook: ', invoice);
-
-            // get last sent date
-            let lastSentDateObj = _.find(invoice.CustomField, {
-              'Name': ENV_CONFIG.QBO_SENT_LABEL
+          qbo.getInvoice(itemId, (err, invoice) => {
+            mailItem(invoice, eventType).then((response) => {
+              res.status(200).send(response);
+            }).catch((error) => {
+              res.status(500).send(error);
             });
-            let lastSentDate = lastSentDateObj ? lastSentDateObj.StringValue : null;
-
-            console.log('last sent date', lastSentDate);
-            console.log('send invoice?', !lastSentDate);
-
-            if (lastSentDate) {
-              res.status(200).send('No need to send...');
-            }
-
-            if (!lastSentDate) {
-              // send the invoice
-              invoiceMailer.send(invoice.Id, eventType).then(() => {
-                res.status(200).send(`Invoice #${invoice.DocNumber} sent!`);
-              }).catch((err) => {
-                res.status(500).send(`Error sending invoice #${invoice.DocNumber}...`);
-              });
-            }
           });
         }
 
         // If its an estimate
         if (eventName === 'estimate') {
-          // get estimate details
-          qbo.getEstimate(eventId, (err, estimate) => {
-            console.log('got estimate details from webhook: ', estimate);
-
-            // get last sent date
-            let lastSentDateObj = _.find(estimate.CustomField, {
-              'Name': ENV_CONFIG.QBO_SENT_LABEL
+          qbo.getEstimate(itemId, (err, estimate) => {
+            mailItem(estimate, eventType).then((response) => {
+              res.status(200).send(response);
+            }).catch((error) => {
+              res.status(500).send(error);
             });
-            let lastSentDate = lastSentDateObj ? lastSentDateObj.StringValue : null;
-
-            console.log('last sent date', lastSentDate);
-            console.log('send estimate?', !lastSentDate);
-
-            if (!lastSentDate) {
-              // send the estimate
-              invoiceMailer.send(estimate.Id, eventType).then(() => {
-                res.status(200).send(`Estimate #${estimate.DocNumber} sent!`);
-              }).catch((err) => {
-                res.status(500).send(`Error sending estimate #${estimate.DocNumber}...`);
-              });
-            }
           });
         }
       });
--- routes/utils/qbo.js
+++ routes/utils/qbo.js
@@ -1,3 +1,6 @@
+let _ = require('lodash');
+let billingItemMailer = require('../admin/emails/billingItem/init');
+
 function processItemData(item) {
   return new Promise((resolve, reject) => {
     if (!item) {
@@ -17,3 +20,33 @@ function processItemData(item) {
   });
 };
 module.exports.processItemData = processItemData;
+
+
+function mailItem(item, eventType) {
+  return new Promise((resolve, reject) => {
+    if (!item) {
+      reject();
+    }
+
+    console.log('got item details from webhook: ', item);
+
+    // get last sent date
+    let lastSentDateObj = _.find(item.CustomField, {
+      'Name': ENV_CONFIG.QBO_SENT_LABEL
+    });
+    let lastSentDate = lastSentDateObj ? lastSentDateObj.StringValue : null;
+
+    console.log('last sent date', lastSentDate);
+    console.log('send item?', !lastSentDate);
+
+    if (!lastSentDate) {
+      // send the item
+      billingItemMailer.send(item.Id, eventType).then(() => {
+        resolve(`Billing item #${item.DocNumber} sent!`);
+      }).catch((err) => {
+        reject(`Error sending billing item #${Item.DocNumber}...`);
+      });
+    }
+  });
+};
+module.exports.mailItem = mailItem;
