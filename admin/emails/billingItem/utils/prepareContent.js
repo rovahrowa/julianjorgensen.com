e7da5472d79649ff4a6a9324d7f11b74799d2af6
Julian Jorgensen <me@julianjorgensen.com>
2017-09-14 12:26:59 +0800

    Invoice

--- admin/emails/billingItem/utils/prepareContent.js
+++ admin/emails/billingItem/utils/prepareContent.js
@@ -76,7 +76,7 @@ let prepareContent = ({
     itemTemplate = 'estimate.pug';
     let expirationDate = moment(item.ExpirationDate, 'YYYY-MM-DD').fromNow();
     let expirationDateFormatted = moment(item.ExpirationDate, 'YYYY-MM-DD').format('MMMM Do, YYYY');
-    emailContext.mainContent = `As promised, here's the estimate ${projectName ? `for ${projectName}` : 'you requested'}. You can view it by clicking the button below.${expirationDate ? ` Please note that it expires ${expirationDate} (${expirationDateFormatted}).` : ''}`}
+    emailContext.mainContent = `As promised, here's the estimate ${projectName ? `for ${projectName}` : 'you requested'}. You can view it by clicking the button below.${expirationDate ? ` Please note that it expires ${expirationDate} (${expirationDateFormatted}).` : ''}`;
   }else{
     // invoice
     switch (eventType) {
