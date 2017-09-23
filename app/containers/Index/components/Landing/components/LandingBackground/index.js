17dae0c4456998b4c674222ac5dab9b51a9ba27d
Julian Jorgensen <me@julianjorgensen.com>
2017-09-23 16:01:36 +0800

    Add homepage bg video

---
+++ app/containers/Index/components/Landing/components/LandingBackground/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './index.css';
+
+export default class LandingBackground extends React.Component {
+  render() {
+    let { position } = this.props.position || {};
+    let dynamicOverlayStyles = {}
+
+    if (position) {
+      let splitWindowWidth = window.innerWidth/2;
+
+      let coordinates = position;
+      let maxPercentage = 1;
+      let positionPercentage = (coordinates.x/splitWindowWidth)*maxPercentage;
+      let leftPositionPercentage = maxPercentage-positionPercentage;
+      let rightPositionPercentage = positionPercentage-maxPercentage;
+
+      dynamicOverlayStyles = {
+        background: `linear-gradient(90deg, rgba(0, 0, 0, ${rightPositionPercentage}) 45%, rgba(0, 0, 0, ${leftPositionPercentage}) 55%)`
+      }
+    }
+
+    return (
+      <div className={styles.wrapper}>
+        <video loop autoPlay controls className={styles.video}>
+          <source src='/videos/julian-jorgensen-northern-lights.mp4' type='video/mp4' />
+          <source src='/videos/julian-jorgensen-northern-lights.webm' type='video/webm' />
+        </video>
+        <div className={styles.overlay} style={dynamicOverlayStyles}></div>
+      </div>
+    )
+  }
+}
