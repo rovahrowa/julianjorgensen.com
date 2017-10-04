147eac499690927980ade44aaf09a98b73b66bab
Julian Jorgensen <me@julianjorgensen.com>
2017-10-04 18:17:10 +0800

    Add homepage mobile styles

--- client/containers/Index/mobile/Landing/components/Slide/index.js
+++ client/containers/Index/mobile/Landing/components/Slide/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import cn from 'classnames';
-import PlayIcon from '-!svg-react-loader?name=Icon!assets/icons/FontAwesome/solid/play-circle.svg';
 import styles from './index.css';
 
 export default class HomeLandingMobileSlide extends React.Component {
   render() {
-    let { title, content, className, playButtonClassName, titleClassName } = this.props;
+    let { title, content, className, playButtonClassName, titleClassName, videoIsLoading } = this.props;
     let _slideStyles = cn(styles.slide, className);
     let _headerStyles = cn(styles.header, titleClassName);
-    let _playButtonStyles = cn(styles.play, playButtonClassName);
     return (
       <div className={_slideStyles}>
         <div className={styles.container}>
@@ -16,7 +14,6 @@ export default class HomeLandingMobileSlide extends React.Component {
             <h1 className={_headerStyles}>{title}</h1>
             {content}
           </div>
-          <div className={_playButtonStyles} onClick={() => alert('play video')}><PlayIcon /></div>
         </div>
       </div>
     )
--- client/containers/Index/mobile/Landing/index.js
+++ client/containers/Index/mobile/Landing/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import cn from 'classnames';
 import Carousel from 'rmc-nuka-carousel';
+import PlayIcon from '-!svg-react-loader?name=Icon!assets/icons/FontAwesome/solid/play-circle.svg';
+import LoadingSpinner from 'components/LoadingSpinner';
 import YouTube from 'react-youtube';
 import Slide from './components/Slide';
 import styles from './index.css';
@@ -18,10 +21,43 @@ let slides = [{
 }]
 
 export default class HomeLandingMobile extends React.Component {
+  state = {
+    activeSlide: 0
+  };
+
+  onVideoReady = (event) => {
+    this.video = event.target;
+  }
+
+  onVideoStateChange = (event) => {
+    switch (event.data) {
+      case 3:
+        this.setState({
+          videoIsLoading: true
+        });
+        break;
+      default:
+        this.setState({
+          videoIsLoading: false
+        });
+    }
+  }
+
+  handleSlideChange = (prevSlide, nextSlide) => {
+    this.setState({
+      activeSlide: nextSlide
+    });
+  }
+
   render() {
+    let { activeSlide, videoIsLoading } = this.state;
+    let _ctaStyles = cn(styles.cta, {
+      [styles.white]: activeSlide === 1 || activeSlide === 2
+    });
+
     return (
       <div className={styles.wrapper}>
-        <Carousel className={styles.slider} decorators={[Dots]} wrapAround={true} autoplay speed={800} autoplayInterval={5000}>
+        <Carousel className={styles.slider} decorators={[Dots]} beforeSlide={this.handleSlideChange} wrapAround={true} autoplay speed={800} autoplayInterval={5000}>
         {slides.map((slide, index) => {
           return (
             <Slide
@@ -30,24 +66,29 @@ export default class HomeLandingMobile extends React.Component {
               titleClassName={styles.title}
               content={slide.content}
               className={styles[`slide${index+1}`]}
-              playButtonClassName={styles.playButton}
             />
           )
         })}
         </Carousel>
-        <YouTube
-          videoId="_OJzg063OyI"
-          className={styles.player}
-          opts={{
-            playerVars: { 
-              autoplay: 0,
-              modestbranding: 1,
-              showinfo: 0,
-              rel: 0
-            }
-          }}
-        />
 
+        <div className={_ctaStyles}>
+          {videoIsLoading ? <LoadingSpinner /> : <PlayIcon />}
+
+          <YouTube
+            videoId="_OJzg063OyI"
+            className={styles.player}
+            opts={{
+              playerVars: { 
+                autoplay: 0,
+                modestbranding: 1,
+                showinfo: 0,
+                rel: 0
+              }
+            }}
+            onReady={this.onVideoReady}
+            onStateChange={this.onVideoStateChange}
+          />
+        </div>
       </div>
     )
   }
