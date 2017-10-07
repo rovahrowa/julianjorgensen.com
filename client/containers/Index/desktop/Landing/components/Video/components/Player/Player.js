6734098869f1906731563d5164d0e3aa48cef29b
Julian Jorgensen <me@julianjorgensen.com>
2017-10-07 23:27:34 +0800

    Load optimizaiton

--- client/containers/Index/desktop/Landing/components/Video/components/Player/Player.js
+++ client/containers/Index/desktop/Landing/components/Video/components/Player/Player.js
@@ -9,18 +9,6 @@ import YouTube from 'react-youtube';
 export default class Player extends React.Component {
   render() {
     let { onReady, styles } = this.props;
-    return <YouTube
-    videoId="_OJzg063OyI"
-    className={styles}
-    opts={{
-      playerVars: { 
-        autoplay: 0,
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0
-      }
-    }}
-    onReady={onReady}
-  />
+    return (<div></div>)
   }
 }
