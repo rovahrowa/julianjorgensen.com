4e12a58640271f310403c6b08dabc549161451b7
Julian Jorgensen <me@julianjorgensen.com>
2017-10-07 19:21:04 +0800

    Load optimizaiton

---
+++ client/containers/Index/desktop/Landing/components/Video/components/Player/Player.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+export default function Player({ onReady, styles }) {
+  return <YouTube
+      videoId="_OJzg063OyI"
+      className={styles}
+      opts={{
+        playerVars: { 
+          autoplay: 0,
+          modestbranding: 1,
+          showinfo: 0,
+          rel: 0
+        }
+      }}
+      onReady={onReady}
+    />
+}
