6cf295cd80b34211a8261b43c11c2985662f7c28
Julian Jorgensen <me@julianjorgensen.com>
2018-01-02 21:48:17 +0100

    Update landing bg webm video

--- client/containers/Index/desktop/Landing/components/BgVideo/index.js
+++ client/containers/Index/desktop/Landing/components/BgVideo/index.js
@@ -5,7 +5,7 @@ import styles from './index.css';
 export default () => (
   <div className={styles.wrapper}>
     <video loop autoPlay className={styles.video} poster={videoPosterImage}>
-      <source src="/videos/vid.mp4" type="video/mp4" />
+      <source src="/videos/julian-jorgensen-northern-lights.mp4" type="video/mp4" />
       <source src="/videos/julian-jorgensen-northern-lights.webm" type="video/webm" />
     </video>
   </div>
